4ed0c8d45f21205bdcc5f44d13ff29700ad6b141
Juan Manuel García del Toro <juanmag.toro@gmail.com>
2024-03-05 21:20:32 +0100

    delete node modules

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,18 +2,25 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const salt = 10;
 
-const userSchema = new mongoose.Schema(
-    {
-        name: { type: String, unique: true, trim: true, required: true },
-        password: { type: String, trim: true, required: true }
-    }
-);
+const userSchema = new mongoose.Schema({
+  name: { type: String, unique: true, trim: true, required: true },
+  password: { type: String, trim: true, required: true },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Email address is required"],
+    validate: {
+      validator: validatorPackage.isEmail,
+      message: "Please provide a valid email",
+    },
+  },
+});
 
 userSchema.pre("save", (next) => {
-    if (this.password) {
-        this.password = bcrypt.hashSync(this.password, salt);
-    }
-    next();
+  if (this.password) {
+    this.password = bcrypt.hashSync(this.password, salt);
+  }
+  next();
 });
 
 const User = mongoose.model("users", userSchema);
