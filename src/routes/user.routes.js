40a58b695c05b0203e0feef53568878957e56c3b
Mathias Bleuzé <mtbleuz@gmail.com>
2024-03-05 19:25:59 +0100

    bcrypt and jsonwebtoken library

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 const userRouter = express.Router();
-const { createUser, authenticate, logout, getUsers } = require('../controller/user.controller');
-const { isAuth } = require('../middlewares/auth.middleware');
+const {
+  createUser,
+  authenticate,
+  logout,
+  getUsers,
+} = require("../controllers/user.controller");
+const { isAuth } = require("../middlewares/auth.middleware");
 
-
-userRouter.post('/register', createUser);
-userRouter.post('/authenticate', authenticate); // obtener un token
-userRouter.post('/logout', [isAuth], logout);
-userRouter.get('/users', getUsers); //este hay que borrarlo antes de pasar a producción.
+userRouter.post("/register", createUser);
+userRouter.post("/authenticate", authenticate); // obtener un token
+userRouter.post("/logout", [isAuth], logout);
+userRouter.get("/users", getUsers); //este hay que borrarlo antes de pasar a producción.
 
 module.exports = userRouter;
-
-
-
-
